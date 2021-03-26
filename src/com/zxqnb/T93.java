3e7eed78e3ce8e51340da1f06e0b2399d1f4400c
zxqnb <1182523747@qq.com>
2021-03-26 16:55:27 +0800

    2021-3-26

--- src/com/zxqnb/T93.java
+++ src/com/zxqnb/T93.java
@@ -1,9 +1,52 @@
 package com.zxqnb;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class T93 {
-    public List<String> restoreIpAddresses(String s) {
+    private List<String> ans = new ArrayList<>();
+    private static final int SEG_CNT = 4;
+    private int[] segments = new int[SEG_CNT];
 
+    public List<String> restoreIpAddresses(String s) {
+        dfs(s, 0, 0);
+        return ans;
+    }
+
+    private void dfs(String s, int segStart, int segId) {
+        if (segId == SEG_CNT) {
+            if (segStart == s.length()) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < SEG_CNT; i++) {
+                    sb.append(segments[i]);
+                    sb.append('.');
+                }
+                sb.deleteCharAt(sb.length() - 1);
+                ans.add(sb.toString());
+            }
+            return;
+        }
+        if (s.length() == segStart) {
+            return;
+        }
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segStart + 1, segId + 1);
+        } else {
+            int addr = 0;
+            for (int segEnd = segStart; segEnd < s.length(); segEnd++) {
+                addr = addr * 10 + (s.charAt(segEnd) - '0');
+                if (addr <= 0xFF) {
+                    segments[segId] = addr;
+                    dfs(s, segEnd + 1, segId + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new T93().restoreIpAddresses("25525511135");
     }
 }
